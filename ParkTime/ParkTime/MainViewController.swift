d4a36c7052eb155ba53243b07bd55adcc9f03a5d
Marcus Hooper <hooperarma@gmail.com>
2015-08-29 00:27:37 +1000

    Adding place picker

---
+++ ParkTime/ParkTime/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  ParkTime
+//
+//  Created by Marcus on 28/08/2015.
+//  Copyright © 2015 easygoingapps. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MainViewController: UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func pickLocation(sender: AnyObject)
+    {
+        let center = CLLocationCoordinate2DMake(51.5108396, -0.0922251)
+        let northEast = CLLocationCoordinate2DMake(center.latitude + 0.001, center.longitude + 0.001)
+        let southWest = CLLocationCoordinate2DMake(center.latitude - 0.001, center.longitude - 0.001)
+        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+        let config = GMSPlacePickerConfig(viewport: viewport)
+        let placePicker = GMSPlacePicker(config: config)
+        
+        placePicker?.pickPlaceWithCallback({ (place: GMSPlace?, error: NSError?) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let place = place {
+                print("Place name \(place.name)")
+                print("Place address \(place.formattedAddress)")
+                print("Place attributions \(place.attributions)")
+            } else {
+                print("No place selected")
+            }
+        })
+    }
+}
+
